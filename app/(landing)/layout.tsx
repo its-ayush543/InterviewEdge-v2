6efc517dfbcc10519a125ae5aca99087ad001cb6
Ayush <ayushlucky2003@gmail.com>
2025-07-27 23:37:33 +0530

    error fix

--- app/(landing)/layout.tsx
+++ app/(landing)/layout.tsx
@@ -1,9 +1,7 @@
-import React from "react";
-
-export default function LandingLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return <>{children}</>;
+export default function LandingLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
 }
